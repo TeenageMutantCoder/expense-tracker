86ea9df89c60bac019b5606ca751f7cc74335309
Stevon Wright <stevonisepic@gmail.com>
2021-12-05 22:22:57 -0500

    Made /api/user return authenticated user

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,19 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nc from "next-connect";
+import { StatusCodes } from "http-status-codes";
 import passport from "../../lib/passport";
+import { IUser } from "../../models/User";
+interface ExtendedRequest {
+  user: IUser;
+}
 
 const userHandler = nc<NextApiRequest, NextApiResponse>({
   onError: (err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).end("Something broke!");
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something broke!" });
   },
   onNoMatch: (req, res, next) => {
-    res.status(404).end("Page is not found");
+    res.status(StatusCodes.NOT_FOUND).json({ msg: "Page is not found" });
   },
 })
   .use(passport.initialize())
-  .post(passport.authenticate("jwt", { session: false }), (req, res) => {
-    res.send("Hello world");
-  });
+  .get<ExtendedRequest>(
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      if (!req.user) {
+        res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ msg: "Authentication error." });
+        return;
+      }
+      res.status(StatusCodes.OK).json({ data: req.user });
+    }
+  );
 
 export default userHandler;
