8633040c5d0f83202f4abd470146866e79647ef3
Stevon Wright <stevonisepic@gmail.com>
2021-12-10 19:02:49 -0500

    Exported IExpense, made expense date and name optional

--- models/Expense.ts
+++ models/Expense.ts
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
-interface IExpense {
-  name: string;
+export interface IExpense {
+  name?: string;
   cost: number;
-  date: Date;
+  date?: Date;
   tags?: string[];
   user: mongoose.ObjectId;
 }
 
 const expenseSchema = new mongoose.Schema<IExpense>({
-  name: { type: String, default: "" },
+  name: String,
   cost: {
     type: Number,
     min: [0, "Cost must be greater than 0"],
     required: true,
   },
-  date: { type: Date, required: true },
+  date: { type: Date },
   tags: [String],
   user: {
     type: mongoose.Schema.Types.ObjectId, // The ID of the User that created the expense
